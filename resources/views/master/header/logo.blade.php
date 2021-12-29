3e4550b2e03d0d86055852e46650b3fee7030d3f
unknown <teugaulrich@gmail.com>
2021-12-29 07:04:48 +0100

    modification du logo

--- resources/views/master/header/logo.blade.php
+++ resources/views/master/header/logo.blade.php
@@ -2,7 +2,8 @@
     <div class="container h-100">
         <div class="row h-100 align-items-center">
             <div class="col-12">
-                <a href="/" class="original-logo"><img src="img/logo.png" alt=""><h2>Imperial</h2></a>
+                {{-- <a href="/" class="original-logo"><img src="img/logo.png" alt=""><h2>Imperial</h2></a> --}}
+                <h2>Imperial</h2>
             </div>
         </div>
     </div>
